436f6ba1bbbd4359bf12cd8f8c5bcf9afcfdc7f7
FireLion137 <55352707+FireLion137@users.noreply.github.com>
2021-03-19 13:19:01 +0100

    Server.cpp v1.3 - Client.cpp v1.3
    In Client.cpp:
        - Aggiunte due variabili string per provare il carattere di controllo sul COM;
        - Rimosse varie librerie e variabili inutili/inutilizzate;
        - Rimossi/Aggiunti alcuni commenti/comandi per controllo;
        - Aggiunta l' Inizializzazione WSA (Winsock) e il Controllo errore di creazione all'inizio del void Invia() -- Risolve il problema per cui il client non si connette;
        - Inserito un loop while(1) all'interno dell' int main() per provare più volte la connessione   **;
             **= WSACleanup() messo a commento perché se viene usato il recv() nel Server.cpp dà valore=-1.
    
    In Server.cpp:
        - Rimosse varie variabili inutili/inutilizzate;
        - Rimossi/Aggiunti alcuni commenti/comandi per controllo;
    
    Problemi: Nel void Carica(), non capisco come dovrebbe funzionare, di base vi è ios::in, ma se non si mette out non crea il file.

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,11 @@
+#define DEBUG
+//#define Type_Server
+#define Type_Client
+
 #include <winsock.h>
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <fstream>
 #include <unistd.h>
 
 using namespace std;
@@ -10,20 +13,28 @@ using namespace std;
 SOCKET remoteSocket;
 short port;
 
-SOCKET clientsocket;
+SOCKET clientSocket;
 SOCKADDR_IN addr;
 
 char config[100];
-
-//SOCKET server
+string s="43#25d#D#55#35##";
+string c="43#25c#D#55#35##";
 
 /********************************************* prototipo funzione invia ***************************************/
 void invia(){
-	//WSA (non usato)
-	//WORD wVersionRequested = MAKEWORD(2,2);
-	//WSADATA wsaData;      
-	//WSAStartup(wVersionRequested, &wsaData);
 	
+	//Inizializzazione WSA (Winsock)
+	WORD wVersionRequested = 0x0202;
+    WSADATA wsaData;   
+    int wsastartup = WSAStartup(wVersionRequested, &wsaData);
+    
+    //Controllo errore di creazione
+	if (wsastartup != NO_ERROR) 
+	{
+		cout << "Errore WSAStartup()" << endl;
+		return;
+	}
+ 
 	//porta del server in ascolto
 	port = 4000;
     
@@ -32,20 +43,15 @@ void invia(){
     //AF_INET fa riferimento alla famiglia di indirizzi IPv4
     addr.sin_family = AF_INET;
     //Inserimento dell'indirizzo IPv4 del server (statico)
-    addr.sin_addr.s_addr = inet_addr("192.168.0.106");
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     //Porta di riferimento della socket del server (qualsiasi, anche se è consigliabile una porta effimera)
     addr.sin_port = htons(port);
     
-    //Creazione socket del client. 
-    //	AF_INET     -> famiglia di indirizzi IPv4
-    //	SOCK_STREAM -> è usato per specificare una socket stream (flusso dati)
-    //  IPPROTO_TCP -> si usa il protocollo TCP (orientato alla connessione)
-    clientsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     
-       //server = connect(clientsocket,(LPSOCKADDR)(&addr),sizeof(addr));
-       //cout<<server;
+    
     //Si tenta di connettersi al server
-    if (connect(clientsocket, (LPSOCKADDR) &addr, sizeof(addr)) < 0)
+    if (connect(clientSocket, (LPSOCKADDR) &addr, sizeof(addr)) < 0)
     {
     	cout << "Errore nella connessione con il Server" << endl;
 	}
@@ -56,11 +62,31 @@ void invia(){
     #endif
     
     //Invia i dati al server
-    send(remoteSocket, config, sizeof(config), 0);
+    send(clientSocket, config, sizeof(config), 0);
     
 }
 
 int main()
 {
-	invia();
+	while(1)
+	{   
+		//Inserisco in config una stringa di caratteri
+		for(int i=0;i<s.length();i++)
+        	config[i]=s[i];
+        
+    	#ifdef Type_Client
+			invia();
+			#ifdef DEBUG
+				cout << "Chiudo il Client" << endl;
+			#endif
+			//WSACleanup();   //Non funziona se lo uso
+			sleep(5);
+		    system("cls");
+	    #endif
+	    sleep(5);
+	}
+   
+	system("pause");
+    return 0;
+    
 }
--- Server.cpp
+++ Server.cpp
@@ -1,24 +1,9 @@
-/****************************************************************************************************************
-*                                                                                                               *
-*           Autori:                             Canzolino Gianluca                                              *
-*                                                                                                               *
-*                                                                                                               *
-*                                                                                                               *
-*           Progetto:                             Smart Room                                                    *
-*                                                                                                               *
-*                                                                                                               *
-*           Verisione:                               v1.4                                                       *
-*                                                                                                               *
-*           Data:                                  28/02/2020                                                   *
-*                                                                                                               *
-*                                                                                                               *
-*****************************************************************************************************************/
+
 
 /******************************************* dichiarazioni costanti *********************************************/
 
 #define DEBUG
 #define Type_Server
-//#define Type_Client
 
 /********************************************* inclusione librerie **********************************************/
 #include <winsock.h>
@@ -47,69 +32,16 @@ char ACK[100]={'P','1','A','1','#','Z'};
 char buffer[256],config[100];     //id_aula[5],lung_trama[3],cod_com[2],temp[5],umid[5],amp[5],lux[5],man_aut[2],ora[7],s_pres[5],s_lux[5],tim_i[7],tim_f[7];
 int wsastartup;
 int ls_result;
-int lunghezza1,n,contatore=0;
 fstream pacchetto;
 
-/********************************************* prototipi funzioni *********************************************/
 
-//Funzione per inviare dati al server
-void invia();
 
-//Salvataggio pacchetto su file di testo
-void salva();
-
-//Caricamento pacchetto su file di testo
-void carica();
-
-//
-void ricezione();
-
-/********************************************* prototipo funzione invia ***************************************/
-void invia(){
-	//WSA (non usato)
-	//WORD wVersionRequested = MAKEWORD(2,2);
-	//WSADATA wsaData;      
-	//WSAStartup(wVersionRequested, &wsaData);
-	
-	//porta del server in ascolto
-	port = 4000;
-    
-    //Dichiarazione tipologia del formato e del valore dell'indirizzo IPv4
-    
-    //AF_INET fa riferimento alla famiglia di indirizzi IPv4
-    addr.sin_family = AF_INET;
-    //Inserimento dell'indirizzo IPv4 del server (statico)
-    addr.sin_addr.s_addr = inet_addr("192.168.1.104");
-    //Porta di riferimento della socket del server (qualsiasi, anche se è consigliabile una porta effimera)
-    addr.sin_port = htons(port);
-    
-    //Creazione socket del client. 
-    //	AF_INET     -> famiglia di indirizzi IPv4
-    //	SOCK_STREAM -> è usato per specificare una socket stream (flusso dati)
-    //  IPPROTO_TCP -> si usa il protocollo TCP (orientato alla connessione)
-    clientsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    
-    //Si tenta di connettersi al server
-    if (connect(clientsocket, (LPSOCKADDR) &addr, sizeof(addr)) < 0)
-    {
-    	cout << "Errore nella connessione con il Server" << endl;
-	}
-	
-	//Stampa di debug
-	#ifdef DEBUG
-    cout << "Messaggio da Inviare: "<<config << endl;
-    #endif
-    
-    //Invia i dati al server
-    send(remoteSocket, config, sizeof(config), 0);
-    
-}
-
-/********************************************* prototipo funzione salva ***************************************/
+/********************************************* prototipo funzione salva - Salvataggio pacchetto su file di testo ************************************/
 void salva(){
-	int valore=0,i;
+	int i;
 	int lunghezza=0;
 	
+	cout<<"\nSono nel void salva()\n";
 	for(i=0;i<256;i++)
     {
     	lunghezza++;
@@ -119,53 +51,47 @@ void salva(){
         	break;
 		}
     }
-    
-	//Semplice ciclo for per salvare su file di testo
-//	do
-//	{
-		pacchetto.open("File/pacchetto.txt",ios::out);
-		if(pacchetto.fail()){
+
+		pacchetto.open("Pacchetto.txt",ios::out);
+		if(pacchetto.fail())
 			cout<<"Errore\n";
-		}
-		else{
+		else
+		{
 			for(i=0;i<lunghezza;i++)
 				pacchetto<<buffer[i];
 			
 			pacchetto<<endl;
-			valore=1;
+			
 		}
-//	}while((pacchetto.fail())&&valore==0);
 	pacchetto.close();
 	
 }
 
-/********************************************* prototipo funzione carica **************************************/
+/********************************************* prototipo funzione carica - Caricamento pacchetto su file di testo ***********************************/
 void carica(){
-	int valore=0,i;
+	int i;
 	
-	//Semplice ciclo do per caricare da file di testo
-	do
-	{
-		pacchetto.open("File/configurazione.txt",ios::in);
-		if(pacchetto.fail()){
+	cout<<"\nSono nel void carica()\n";
+
+		pacchetto.open("Configurazione.txt",ios::in);
+		if(pacchetto.fail())
 			cout<<"Errore\n";
-		}
-		else{
+		else
+		{
 			pacchetto>>config[i];
-			while (!pacchetto.eof()){
+			while (!pacchetto.eof())
+			{
 				i++;
 				pacchetto>>config[i];
 			}
-			valore=1;
-			lunghezza1=i+1;
 		}
-	}while((pacchetto.fail())&&valore==0);
+
 	pacchetto.close();
 }
 
-/********************************************* prototipo funzione ricezione ***********************************/
+/********************************************* prototipo funzione ricezione - Ricezione dati dal Client *********************************************/
 void ricezione(){
-	int valore;
+	int i;
 	
 	//Inizializzazione WSA (Winsock)
 	WORD wVersionRequested = 0x0202;
@@ -183,13 +109,13 @@ void ricezione(){
     listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     
     //Controllo creazione della socket
-	if (listenSocket < 0) {
+	if (listenSocket < 0)
+	{
 		cout << "Server: errore nella creazione del socket." << endl;
 		return;
 	}
-    else {
+    else
     	cout << "il Socket e' pronto"<< endl;
-	}
         
     //Porta di ascolto della socket
     port = 4000;
@@ -197,12 +123,13 @@ void ricezione(){
     //AF_INET fa riferimento alla famiglia di indirizzi IPv4
     Server_addr.sin_family = AF_INET;
     //Inserimento dell'indirizzo IPv4 del server (statico)
-    Server_addr.sin_addr.s_addr = inet_addr("192.168.0.106");
+    Server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     //Porta di riferimento della socket del server (qualsiasi, anche se è consigliabile una porta effimera)
     Server_addr.sin_port = htons(port);
      
     //Una bind (cioè collegamento) della socket in modo tale da rendersi disponibile ad ascoltare
-    if (bind(listenSocket, (LPSOCKADDR) &Server_addr, sizeof(Server_addr)) < 0) {
+    if (bind(listenSocket, (LPSOCKADDR) &Server_addr, sizeof(Server_addr)) < 0)
+	{
     	cout << "Server: errore durante la bind." << endl;
         closesocket(listenSocket);
         system("pause");
@@ -225,8 +152,6 @@ void ricezione(){
     
     //Dimensione della struttura sockaddr_in (per avere l'area effettiva usata dalla struttura dati)
     sin_size = sizeof(struct sockaddr_in);
-    
-    
     //Accettazione del client che tenta di comunicare con il server
     remoteSocket = accept(listenSocket, (LPSOCKADDR) &Client_addr, &sin_size);
     //Stampa dell'accettazione con un client
@@ -242,60 +167,16 @@ void ricezione(){
     //inserimento nel buffer del pacchetto ricevuto
     recv(remoteSocket, buffer, sizeof(buffer), 0);
     
- /*   Metto a commento in quanto sostituisco il tutto con un controllo del carattere COM nel buffer
-    //controllo del messaggio (lunghezza maggiore 10 è una richiesta, minore è un ack)
-    int stato=0;
-	
-    for(i=0;i<86;i++)
-    {
-    	if(buffer[i]=='#')
-    	{
-    		stato++;
-    		lunghezza++;
-		}
-		else if(stato!=2)
-		{
-			lunghezza++;
-			stato=0;
-		}else{
-			break;
-		}
-	}
-	
-	#ifdef DEBUG
-    cout << "Messaggio Arrivato: " << buffer <<endl<<lunghezza<< endl;
-    #endif
-    
-    //se >10 è un messaggio, quindi lo salvo richiamando la funzione salva()
-    if(lunghezza>10)
-	{
-    	salva();
-    	cout<<"Salva"<<endl;
-    	sleep(1);
-    	//invio un ACK di avvenuta ricezione
-		send(remoteSocket, ACK, sizeof(ACK), 0);
-		sleep(1);
-	}
-	//se <10 è un ack, quindi carico il messaggio e spedisco il pacchetto
-	else
-	{
-		sleep(4);
-		cout<<"Carica"<<endl;
-		carica();
-		cout<<config<<endl<<sizeof(config)<<endl;
-		sleep(1);
-		//invio il pacchetto
-		send(remoteSocket, config, sizeof(config), 0);
-		sleep(1);
-	}
- */
+    	for(i=0;i<256;i++)
+    		cout<<buffer[i];
+
  
  	//controllo del messaggio (Se COM = 'd' è una richiesta, Se COM = 'c' è un ack)
  	//se COM = 'd' è un messaggio, quindi lo salvo richiamando la funzione salva()
 	if(buffer[5]=='d')
 	{
+		cout<<"Salva"<<endl;
    		salva();
-    	cout<<"Salva"<<endl;
     	sleep(1);
     	//invio un ACK di avvenuta ricezione
 		send(remoteSocket, ACK, sizeof(ACK), 0);
@@ -304,7 +185,6 @@ void ricezione(){
 	//se COM = 'c' è un ack, quindi carico il messaggio e spedisco il pacchetto
 	else if(buffer[5]=='c')
 	{
-		sleep(4);
 		cout<<"Carica"<<endl;
 		carica();
 		cout<<config<<endl<<sizeof(config)<<endl;
@@ -317,8 +197,8 @@ void ricezione(){
 }
 
 /********************************************* prototipo funzione main ****************************************/
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     while(1)
 	{      
     	#ifdef Type_Server
@@ -327,10 +207,10 @@ int main(int argc, char *argv[]) {
 				cout << "Chiudo il Server" << endl;
 			#endif
 			WSACleanup(); 
-			sleep(2);
+			sleep(5);
 		    system("cls");
 	    #endif
-	    sleep(30);
+	    sleep(3);
 	}
    
 	system("pause");
